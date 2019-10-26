e79f846dd806f335d34eacff003e0c7e82b70fb5
Gustavo Perenciolo <26912712+perenciolo@users.noreply.github.com>
2019-10-26 16:31:45 -0300

    Update ubuntu-19.x-postinstall.sh

--- ubuntu-19.x-postinstall.sh
+++ ubuntu-19.x-postinstall.sh
@@ -132,14 +132,8 @@ sudo apt autoremove -y
 
 echo -e "\e[32;7m DOCKER INSTALL \e[0m"
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install docker.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
